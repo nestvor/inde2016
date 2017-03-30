ea3460e374d5f3ceae6ea34016c9e6968df410a0
Nejc Ločniškar <nejc.locniskar@gmail.com>
2017-03-30 18:36:55 +0200

    nekaj sem naredil

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<div class="section group latest-cards">
-            <div>IVO IVO IVO IMA SE FINO, KO GRE V MARINO</div>
+            <div>IVO IVO IVO ČAKA NA ZIMO</div>
 		<?php
 			/* display the 3 latest posts */
 			
