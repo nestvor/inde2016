53c67bfe241d866b765865d5820bc702449eb29f
Ivo <ivo.planinsek@hotmail.com>
2017-05-11 21:38:18 +0200

    ivo deleting message

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-        ivotov test lolol
+
 		<div class="section group latest-cards">
             <div class="row">
                 <?php
